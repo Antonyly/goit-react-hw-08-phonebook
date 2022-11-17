3cd9558f5ad58cc6da07703bd95c7496476ccacc
Antonyly <antonlyzun@gmail.com>
2022-11-17 16:05:30 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
-import { store } from './redux/store'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
+      </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { createAction, createReducer } from '@reduxjs/toolkit'
-import { contactsReducer } from './contactsSlice'
-import { filterSlice } from './filterSlice'
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
+import rootReducer from "./root-reducer";
 
 export const store = configureStore({
-    reducer: {
-        contacts: contactsReducer,
-        filter: filterSlice.reducer
-  },
-  
-})
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
